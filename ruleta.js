3ee38a86558b3c0fe32833f6e0d000a31498cda4
maillenandrade-lgtm <maillenandrade@gmail.com>
2026-09-13 18:38:17 -0300

    Update and rename transacciones.js to ruleta.js

---
+++ ruleta.js
@@ -0,0 +1,18 @@
+let saldoActual = 1000;
+const textoSaldo = document.getElementById('saldoUsuario');
+const mensajeExito = document.getElementById('mensajeExito');
+
+document.getElementById('formDeposito').addEventListener('submit', function(e) {
+    e.preventDefault(); 
+    let monto = parseInt(document.getElementById('deposito').value);
+    
+    saldoActual += monto; 
+    textoSaldo.innerText = saldoActual; // Actualiza el número en pantalla
+    document.getElementById('deposito').value = ''; // Limpia el cajón
+    
+    mensajeExito.innerText = "¡Fichas añadidas correctamente!";
+    mensajeExito.classList.remove('d-none');
+    setTimeout(() => {
+        mensajeExito.classList.add('d-none');
+    }, 3000); 
+});
