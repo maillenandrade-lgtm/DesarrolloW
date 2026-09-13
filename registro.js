92ac453a8531f78bdb17face63b5568c015b99b1
maillenandrade-lgtm <maillenandrade@gmail.com>
2026-09-13 17:01:35 -0300

    Create registro.js

---
+++ registro.js
@@ -0,0 +1,13 @@
+document.getElementById('formRegistro').addEventListener('submit', function(evento) {
+    let pass1 = document.getElementById('password').value;
+    let pass2 = document.getElementById('password_confirm').value;
+    let errorMsg = document.getElementById('errorMsg');
+
+    if (pass1 !== pass2) {
+        evento.preventDefault(); 
+        errorMsg.classList.remove('d-none'); 
+    } else {
+        errorMsg.classList.add('d-none');
+        alert("Registro exitoso. Redirigiendo al panel..."); 
+    }
+});
